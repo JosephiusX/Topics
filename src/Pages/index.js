4963cb4faffc2f3f6cf71402458c759197b0d157
josephius x <josephiusx@gmail.com>
2023-01-09 23:59:39 -0800

    clean slate

--- src/Pages/index.js
+++
@@ -1,2 +0,0 @@
-export * from './TopicsPage'
-export * from './ErrorPage'
--- src/main.jsx
+++ src/main.jsx
@@ -3,30 +3,6 @@ import ReactDOM from "react-dom/client";
 import {Route, Switch, RouterProvider, BrowserRouter} from "react-router-dom";
 import {TopicsPage, ErrorPage } from "./Pages"
 
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <TopicsPage />,
-//     errorElement: <ErrorPage />,
-//   }
-// ]);
-
-const AppRouter = () => (
-  <BrowserRouter>
-    <div>
-      {/* <Header /> */}
-      <Switch> 
-        {/* TOPICS */}
-        <Route path="/" component={TopicsPage} exact={true} />
-        <Route component={ErrorPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={AppRouter} />
